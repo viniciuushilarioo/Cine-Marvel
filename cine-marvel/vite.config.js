760a8f6176a80f922853b93149ffdaed0a3806ab
duardovinicius <duardovinicius@gmail.com>
2025-05-26 08:23:12 -0300

    a

--- cine-marvel/vite.config.js
+++ cine-marvel/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+module.exports = {
+  pwa: {
+    name: 'ComicVerse',
+    themeColor: '#ff0000',
+    background_color: '#000000',
+    display: 'standalone',
+    start_url: '.',
+    icons: [
+      {
+        src: './img/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: './img/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ],
+    manifestOptions: {
+      short_name: 'ComicApp',
+      description: 'Quadrinhos Marvel em alta qualidade!',
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  }
+}
