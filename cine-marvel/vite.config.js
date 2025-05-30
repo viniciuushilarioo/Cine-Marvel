76db78fe8c504b61dc28e418256df401126cccd5
duardovinicius <duardovinicius@gmail.com>
2025-05-30 16:41:04 -0300

    cc

--- cine-marvel/vite.config.js
+++ cine-marvel/vite.config.js
@@ -1,6 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'cine-marvel',
+        short_name: 'cine-marvel',
+        description: 'cine-marvel',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        id: 'com.fake-store.app',
+        orientation: 'any',
+        background_color: '#ffffff',
+        start_url: '.',
+        launch_handler: {
+          client_mode: ['navigate-existing', 'auto'],
+        },
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
